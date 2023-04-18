28213ab1b779836bddc3782d7e67c843f6891c7c
sven12ru <vksk12@bk.ru>
2023-04-18 20:54:56 +0300

    изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/jft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/Point.java
@@ -1,11 +1,10 @@
 package ru.stqa.jft.sandbox;
 
 public class Point {
-	
-	public static void main(String[] agrs){
-		
-		System.out.println("Hello, World");
-	}
-	
-	
+
+  public static void main(String[] agrs) {
+    System.out.println("Hello, World");
+  }
+
+
 }
