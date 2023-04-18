78f160df56c9964079a547c0a8fb37ed9ccbc98a
sven12ru <vksk12@bk.ru>
2023-04-18 22:38:39 +0300

    добавлены новые функции

--- sandbox/src/main/java/ru/stqa/jft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/Point.java
@@ -3,12 +3,25 @@ package ru.stqa.jft.sandbox;
 public class Point {
 
   public static void main(String[] agrs) {
-    System.out.println("Hello, World");
+    hello("world");
+    hello("java");
+    hello("kia");
 
-    int a = 6;
-    int s = a * a;
+    double l = 5;
+    System.out.println("площадь квадрата со стороной " + l + " = " + area(l));
+    double a = 6;
+    double b = 8;
+    System.out.println("площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b) );
+  }
+  public static void hello(String somebody){
+   System.out.println("hello, " + somebody + "!");
+  }
 
-    System.out.println("площадь квадрата со стороной " + a + " = " + s );
+  public static double area(double len){
+    return len * len;
+  }
+  public static double area(double a, double b){
+    return a * b;
   }
 
 
