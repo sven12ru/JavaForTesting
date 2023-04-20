e1798c5ba82f74c3ef1684f3b1bdc2bc8723c4ce
sven12ru <vksk12@bk.ru>
2023-04-20 20:44:51 +0300

    перемещение метода в классы

--- sandbox/src/main/java/ru/stqa/jft/sandbox/FirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/FirstProgramm.java
@@ -8,21 +8,17 @@ public class FirstProgramm {
     hello("kia");
 
     Square s = new Square(5);
-    System.out.println("площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(3, 7 );
-    System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r) );
+    System.out.println("площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area() );
   }
   public static void hello(String somebody){
    System.out.println("hello, " + somebody + "!");
   }
 
-  public static double area(Square s){
-    return s.l * s.l;
-  }
-  public static double area(Rectangle r){
-    return r.a * r.b;
-  }
+
+
 
 
 }
