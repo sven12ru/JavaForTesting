6129641c4b021839174472c25f91c87f0b7c509f
sven12ru <vksk12@bk.ru>
2023-05-01 17:53:37 +0300

    убрал текст с уроков

--- sandbox/src/main/java/ru/stqa/jft/sandbox/FirstProgramm.java
+++ sandbox/src/main/java/ru/stqa/jft/sandbox/FirstProgramm.java
@@ -9,7 +9,6 @@ public class FirstProgramm {
       System.out.println("расстояние между точками: " + p1.distance(p2));
 
   }
-
     public static double distance(Point p1, Point p2){
         return Math.sqrt( Math.pow(p2.x - p1.x, 2) +Math.pow(p2.y - p1.y, 2));
     }
--- sandbox/src/test/java/ru/stqa/jft/sundbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/jft/sundbox/PointTest.java
@@ -5,14 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.jft.sandbox.Point;
 
 public class PointTest {
-/*
-    @Test
-    public void testArea() {
-        Square s = new Square(5);
-        Assert.assertEquals(s.area(), 25.0);
 
-    }
-*/
 @Test
     public void testPoint(){
 
